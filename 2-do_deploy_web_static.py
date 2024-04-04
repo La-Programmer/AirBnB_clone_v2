1c50e96271eceee12b5e5d2ff60d6c4ebfb0f216
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-04 17:42:29 +0100

    FIX: Making fabric script pycodestyle compliant

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ env.user = 'ubuntu'
 env.hosts = ['54.209.112.14', '52.91.134.77']
 env.key_filename = '~/.ssh/school'
 
+
 # Deployment function
 def do_deploy(archive_path):
     """Deploys an archive to web servers"""
@@ -19,11 +20,20 @@ def do_deploy(archive_path):
         archive_filename = run('ls /tmp/ | grep ".tgz"')
         extracted_filename = archive_filename.split('.')[0]
         print(archive_filename, extracted_filename)
-        sudo('mkdir /data/web_static/releases/{}'.format(extracted_filename))
-        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_filename, extracted_filename))
+        sudo(
+            'mkdir -p /data/web_static/releases/{}'
+            .format(extracted_filename)
+        )
+        sudo(
+            'tar -xzf /tmp/{} -C /data/web_static/releases/{}'
+            .format(archive_filename, extracted_filename)
+        )
         run('rm /tmp/{}'.format(archive_filename))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s /data/web_static/current /data/web_static/releases/{}'.format(extracted_filename))
+        sudo(
+            'ln -s /data/web_static/current /data/web_static/releases/{}'
+            .format(extracted_filename)
+        )
         return True
     except Exception as e:
         print("An error occurred: ", e)
