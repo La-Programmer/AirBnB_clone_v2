57cc3eb3f810a397bbb0428589199ee1d9139496
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-04 10:46:52 +0100

    FILE: Addition of script 0 to automate web server setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Setting up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt install nginx
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo sh -c 'echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html'
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+export string="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "/server_name _;/a\\$string" /etc/nginx/sites-available/default
+sudo nginx -t
+sudo nginx -s reload
