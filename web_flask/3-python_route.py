0f1bbddd692ca6a728a157e707f0715fe9bb8db3
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-18 16:41:55 +0100

    FILE: Addition of script 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb")
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB!"
 
 
-@app.route("/c/<text>")
-def print_c(text):
+@app.route('/c/<text>', strict_slashes=False)
+def print_c(text=None):
     new_text = text.replace('_', ' ')
-    return "C {}".format(new_text)
+    return "C {}".format(escape(new_text))
 
 
-@app.route("/python/<text>")
-def print_c(text):
+@app.route('/python/<text>', strict_slashes=False)
+def print_py(text):
     new_text = text.replace('_', ' ')
-    return "Python {}".format(new_text)
+    return "Python {}".format(escape(new_text))
 
+
+@app.route('/python/', strict_slashes=False)
+def print_py_default(text="is cool"):
+    new_text = text.replace('_', ' ')
+    return "Python {}".format(escape(new_text))
 
 if __name__ == "__main__":
     app.run(debug='true', host='0.0.0.0')
