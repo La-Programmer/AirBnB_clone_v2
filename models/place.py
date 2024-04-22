f49abf024f242b836d6cbdc8b834c0aedd64cba0
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-22 22:07:53 +0100

    UPDATE: Extending the functionality of review and defining relationship between review, places and users

--- models/place.py
+++ models/place.py
@@ -3,10 +3,12 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
+import os
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
+    db = os.environ.get('HBNB_TYPE_STORAGE')
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'))
     user_id = Column(String(60), ForeignKey('users.id'))
@@ -20,3 +22,20 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     user = relationship("User", back_populates="places")
     cities = relationship("City", back_populates="places")
+    if (db == 'db'):
+        reviews = relationship("Review", back_populates="place")
+    else:
+        @property
+        def reviews(self):
+            """Getter for reviews of a place"""
+            from models import storage
+            list = storage.all(cls='Review')
+            temp = []
+            result = []
+            # DEBUG PRINTING TAKE NOTICE OF THE LINES BELOW
+            # POSSIBLE ERROR
+            print(list)
+            for key, value in list.items:
+                temp.append(str(value))
+            result = [instance for instance in temp if instance.place_id == self.id]
+            return result
--- models/review.py
+++ models/review.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024))
+    place_id = Column(String(60), ForeignKey('places.id'))
+    user_id = Column(String(60), ForeignKey('users.id'))
+    user = relationship("User", back_populates="reviews")
+    place = relationship("Place", back_populates="reviews")
